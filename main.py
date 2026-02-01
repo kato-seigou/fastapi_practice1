b58b47f887b7535f592032d000f66f99a2f2306f
kato-seigou <seigou444@gmail.com>
2026-02-01 13:46:36 +0900

    country関数の作成

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 
 class Data(BaseModel):
     x: int
@@ -16,3 +17,11 @@ async def calc(data: Data):
     z = data.x * data.y
     print(f"received: x={data.x}, y={data.y}")
     return {"result": z}
+
+@app.get("/")
+@app.get("/countries/")
+async def country(country_name: Optional[str] = None, country_no: Optional[int] = None):
+    return {
+        "country_name": country_name,
+        "country_no": country_no
+    }
